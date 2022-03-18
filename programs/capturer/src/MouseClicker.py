67c5f80792602faaa67e95024f13899f0f166069
Ben Mette <benme04@gmail.com>
2022-03-18 21:27:10 +0100

    MouseClicker Simulation adjusted to fit jsBridge

--- programs/capturer/src/MouseClicker.py
+++ programs/capturer/src/MouseClicker.py
@@ -9,9 +9,14 @@ import logging
 import JSONHandler
 import Unpress
 
+import functools
+print = functools.partial(print, flush=True)
 
 _data = None
 listeners = None
+_on_pause = False
+_ending = False
+_timer = None
 
 
 def read_file():
@@ -29,7 +34,9 @@ def run_key_thread():
 
 def on_press(key):
 	if type(key) == keyboard.Key and key == keyboard.Key["f1"]:
-		end()
+		request("stop")
+	if type(key) == keyboard.Key and key == keyboard.Key["f2"]:
+		request("resume") if _on_pause else request("pause")
 
 
 def check_stop_event():
@@ -41,18 +48,98 @@ def check_stop_event():
 
 
 
+# ------------------------- IPC ---------------------------------
+
+
+def request(cmd: str):
+	global _ending
+	if cmd == 'pause' and not _on_pause:
+		pause()
+	if cmd == 'resume' and _on_pause:
+		resume()
+	if cmd == 'stop' and not _ending:
+		_ending = True
+		request("pause")
+		end()
+
+
+def pause():
+	global _on_pause
+	_on_pause = True
+	print("pause")
+
+def resume():
+	global _on_pause
+	_on_pause = False
+	print("resume")
+
+
+# ----------------------- Timer ---------------------------------
+
+def setTimer(timer):
+	_timer = timer
+	if _on_pause:
+		_timer.pause()
+
+
+class Timer:
+	def __init__(self):
+		self._sleep_time = None
+		self._rdy = True
+		self._start = None
+		self._pause_time = None
+		self._pause_amount = 0
+
+	def is_ready(self):
+		return self._rdy
+
+	def pause(self):
+		if self._sleep_time == None: return
+		if not self._pause_time == None: return
+		self._pause_time = time.time()
+
+	def resume(self):
+		if self._sleep_time == None: return
+		self._pause_amount += time.time() - self._pause_time()
+		self._pause_time = None
+
+	def start(self, seconds: int):
+		if not self._rdy: return
+		self._rdy = False
+		self._sleep_time = seconds
+		threading.Thread(target=self._run, daemon=True)
+	
+	def _run(self):
+		while True:
+			if not self._pause_time == None: continue
+			if time.time() - (self._start + self._pause_amount) >= self._sleep_time:
+				break
+		self._rdy = True
+		self._sleep_time = None
+
 # ---------------------- simulation -----------------------------
 
 def simulate_behaviour():
 	delay = 0
-	for instruction in _data.storage:
-		out_time = instruction["time"]-delay
-		if out_time < 0:
-			time.sleep(0)
-		else:
-			time.sleep(out_time)
-			delay = instruction["time"]
-		simulate_instruction(instruction)
+	index = 0
+	while index < len(_data.storage):
+		if(_on_pause): continue
+		if(_ending): break
+		delay = time_exec_instruction(_data.storage[index], delay)
+		index += 1
+
+
+# returns the delay of this operation relativ to start of programm
+def time_exec_instruction(instruction, delay):
+	out_time = instruction["time"]-delay
+	if out_time < 0:
+		time.sleep(0)
+	else:
+		print(out_time)
+		time.sleep(out_time)
+		delay = instruction["time"]
+	simulate_instruction(instruction)
+	return delay
 
 
 def simulate_instruction(instruction: dict):
@@ -88,18 +175,22 @@ def set_configs():
 
 
 def main():
+	print("c")
 	global _data
 	_data = JSONHandler.MetaData(False)
-	set_configs()
+	# set_configs()
+	config_monitor()
 	read_file()
 	run_key_thread()
 	simulate_behaviour()
-	end()
+	request("stop")
 
 
 def end():
 	Unpress.key_press_warnings(_data.keyboard_controller)
 	listeners.stop()
+	print("stop")
+
 
 if __name__ == '__main__':
 	main()
