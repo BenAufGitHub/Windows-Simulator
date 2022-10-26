a0e0947068a7873a8d9f49e6ff980f84eed309f9
BenAufGitHub <benme04@gmail.com>
2022-10-26 16:12:14 +0200

    More Styling For Record-Options (WIP)

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ const record = document.getElementById("record")
 const expand = document.getElementById("expand-saves");
 let input = document.getElementById("save-input");
 
-function styleButton(btn) {
-    btn.style["display"] = "inline-block";
-    btn.style["text-align"] = "center"
-}
+
+// ============================= functionality ===================================
+
 
 const WINDOW_API = {
     pause: (application) => ipcRenderer.send("pause", application),
@@ -27,9 +26,18 @@ const startSimulate = () => {
     }
 }
 
+const addClickEvents = () => {
+    record.onclick = startRecording;
+    simulate.onclick = startSimulate;
+    expand.onclick = expandRecordFiles;
+}
 
-function resolveChooseRecordFile (result) {
-    //TODO
+function resolveChooseRecordFile (result, filenames) {
+    removeRecordSelectOptions();
+    if(result==0)
+        createNewRecording();
+    else
+        selectRecording(filenames[result]);
 }
 
 function getRecordFiles () {
@@ -37,28 +45,18 @@ function getRecordFiles () {
     return ["Boba Fett", "Tyrannus Saurus Rex.exe", "Stevosaurus TV", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m"];
 }
 
-function createButtons (filenames, callbackResultFunc) {
-    let buttons = [];
-    for(let i=0; i<filenames.length; i++){
-        let b = document.createElement("button");
-        b.classList.add("expand-button-option");
-        if(i+1==filenames.length)
-            b.classList.add("no-border");
-        else
-            b.classList.add("record-option-border");
-        b.onclick = () => callbackResultFunc(i);
-        b.innerHTML = filenames[i];
-        buttons.push(b);
-    }
-    buttons[0].classList.add("new-recording-option")
-    return buttons;
-}
 
-function createRecordContainer () {
-    let container = document.createElement("div");
-    container.classList.add("floating-top-right");
-    return container;
-}
+// ============================= Responsiveness =====================================
+
+
+// Popups disappear on blur
+window.addEventListener('mouseup', function(e){
+    if (e.target.classList.contains('expand-button-option') || e.target.classList.contains('floating-top-right')) return;
+    removeRecordSelectOptions();
+})
+
+
+// ===== record creation ====>
 
 const expandRecordFiles = () => {
     let filenames = getRecordFiles();
@@ -66,6 +64,7 @@ const expandRecordFiles = () => {
     let optionButtions = createButtons(filenames, resolveChooseRecordFile);
     container = createRecordContainer();
     addRecordExpansionToDocument(container, optionButtions);
+    container.focus();
 }
 
 const addRecordExpansionToDocument = (container, children) => {
@@ -77,14 +76,68 @@ const addRecordExpansionToDocument = (container, children) => {
 }
 
 
+// ======= record removal ====>
+
+function selectRecording (filename) {
+    input.setAttribute("disabled", "");
+    //TODO actually set Recording
+    setRecordFileInput(filename);
+}
+
 function setRecordFileInput (text) {
     input['value'] = text;
 }
 
-const addClickEvents = () => {
-    record.onclick = startRecording;
-    simulate.onclick = startSimulate;
-    expand.onclick = expandRecordFiles;
+function removeRecordSelectOptions () {
+    let recMenu = document.getElementById('input-field');
+    let container = document.getElementById('record-select-container');
+    if(!container) return;
+    recMenu.removeChild(container);
+}
+
+function createNewRecording() {
+    setRecordFileInput("");
+    input.removeAttribute("disabled");
+    input.focus()
+}
+
+
+// ============================ DOM-Elements =========================================
+
+// ============= RecordOptionButtons ==>
+
+function createButtons (filenames, callbackResultFunc) {
+    let buttons = [];
+    for(let i=0; i<filenames.length; i++){
+        let b = createNewButton(i, filenames, callbackResultFunc);
+        buttons.push(b);
+    }
+    buttons[0].classList.add("new-recording-option")
+    return buttons;
+}
+
+const createNewButton = (i, filenames, callbackResultFunc) => {
+    let b = document.createElement("button");
+    b.classList.add("expand-button-option");
+    if(i+1==filenames.length)
+        b.classList.add("no-border");
+    else
+        b.classList.add("record-option-border");
+    b.onclick = () => callbackResultFunc(i, filenames);
+    b.innerHTML = filenames[i];
+    return b;
+}
+
+// ============== Option Container =============>
+
+function createRecordContainer () {
+    let container = document.createElement("span");
+    container.id = "record-select-container";
+    container.classList.add("floating-top-right");
+    container.setAttribute("contentEditable", "");
+    container.setAttribute("spellcheck", "false");
+    i = document.getElementById("input-field")
+    return container;
 }
 
 
@@ -92,5 +145,5 @@ const addClickEvents = () => {
 main:
 {
     addClickEvents();
-    setRecordFileInput("Select recording")
+    setRecordFileInput("Select recording");
 }
