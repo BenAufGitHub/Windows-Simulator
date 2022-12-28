d9ffbb3efc8b0c983eedd851f8044405d35f54a6
BenAufGitHub <benme04@gmail.com>
2022-12-28 13:53:50 +0100

    Adding new tip to welcome page

--- src/welcome/welcome.js
+++ src/welcome/welcome.js
@@ -22,9 +22,18 @@ function disableElems () {
 function setInfo (lang) {
     let p = document.getElementById('info-text');
     p.removeAttribute('hidden');
-    if(lang=='english')
+    if(lang=='en')
         return p.innerHTML = `Hint: Click \u{1F6C8} in the menu to see all necessary information.`
     p.innerHTML = `Hinweis: Klicke \u{1F6C8} im Men&uuml; zum einsehen aller wichtigen Informationen.`
+}
+
+
+function setInfo2 (lang) {
+    let p = document.getElementById('info-text');
+    p.removeAttribute('hidden');
+    if(lang=='en')
+        return p.innerHTML = `'F2' is an important key: Use it to stop your recordings.`
+    p.innerHTML = `'F2' ist eine m&auml;chtige Taste: Nutze es, um Aufnahmen zu beenden.`
 }
 
 
@@ -32,7 +41,8 @@ function processResults (lang, source) {
     disableElems();
     source.classList.add("is-loading");
     setInfo(lang)
-    setTimeout(()=>sendResult(lang), 5000);
+    setTimeout(()=>setInfo2(lang), 5000);
+    setTimeout(()=>sendResult(lang), 10000);
 }
 
 function addClickEvents () {
