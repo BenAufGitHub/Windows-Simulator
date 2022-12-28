1c3e2a5d816ca609b8e50ec80a1ae7cbd21909f4
BenAufGitHub <benme04@gmail.com>
2022-12-28 13:41:59 +0100

    lang-support for pause page

--- src/pause/pause.js
+++ src/pause/pause.js
@@ -21,3 +21,18 @@ const stopBtn = document.getElementById("stop");
 resumeBtn.onclick = () => sendFromButton("resume")
 stopBtn.onclick = () => sendFromButton("stop")
 
+
+const get = (id) => {
+    return document.getElementById(id);
+}
+
+
+async function addInnerHTML () {
+    let lang_pack = await ipcRenderer.invoke("get-lang-pack", null);
+    get("title").innerHTML = lang_pack["pause"]["title"];
+    get("instructions").innerHTML = `<strong>${lang_pack["pause"]["instructions"]}</strong>`;
+    get("resume").innerHTML = lang_pack["pause"]["resume"];
+    get("stop").innerHTML = lang_pack["pause"]["stop"];
+}
+
+addInnerHTML();
